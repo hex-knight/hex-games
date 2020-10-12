420702f0f9dd443baf5a8afc63b485f6ceb790f3
David Mariscal <david.mariscal.fdez@hotmail.com>
2020-10-11 23:34:41 -0500

    First Flappy thing game

---
+++ bird.js
@@ -0,0 +1,67 @@
+var g = 1;
+var upG = -21;
+function Bird () {
+    this.y = height/2;
+    this.x = width/4;
+    this.velocity = 0;
+    this.isDead = false;
+    this.pipeColor = color(0,200,0,255);
+
+    this.show = function(){
+        stroke(0);
+        beginShape();
+        strokeWeight(2);
+        fill(250,150,0);//body color
+        ellipse(this.x,this.y,30,30);//body
+        stroke(0);
+        strokeWeight(1);
+        fill(255);//eyeball color
+        ellipse(this.x+10,this.y-7,15,15);//eyeball
+        fill(0);//eye color
+        ellipse(this.x+14,this.y-7,4,4);//eye
+        fill(255,130,0);//wing color
+        ellipse(this.x-10,this.y+5,20,15);//wing
+        fill(255,255,0);
+        vertex(this.x+10,this.y);
+        vertex(this.x+15,this.y-2);
+        vertex(this.x+30,this.y);
+        vertex(this.x+15,this.y+8);
+        vertex(this.x+10,this.y);
+        endShape();
+    }
+
+    this.dies = function(){
+        this.isDead = true;
+    }
+
+    this.update = function(){
+        if(!this.isDead){
+            this.fall();
+            if(this.y<=0){
+                this.y = 0;
+                this.velocity = 0;
+            }
+            if(this.y>=height){
+                this.y = height;
+                this.velocity = 0;
+            }
+        }else{
+            if(keyIsPressed){
+                if(key == 'r'){
+                    this.isDead = false;
+                    this.velocity=0;
+                    this.y=height/2;
+                }
+            }
+        }
+    }
+
+    this.fall = function(){
+        this.velocity += g;
+        this.y += this.velocity;
+    }
+
+    this.jump = function(){
+        this.velocity += upG;
+    }
+}
---
+++ pipes.js
@@ -0,0 +1,36 @@
+function Pipes(){
+    this.x = width;
+    this.top = random(height/4)*3;
+    this.sep = 200;
+    this.w = 60;
+    //this.bottom = random(height/2);
+
+    this.show = function(){
+        fill(0,200,0);
+        beginShape();
+        stroke(0);
+        rect(this.x,0,this.w,this.top);//top pipe
+        rect(this.x-5,this.top-30,this.w+10,30); //top pipe end
+        rect(this.x,this.top+this.sep,this.w,height-this.top-this.sep);//top pipe
+        rect(this.x-5,this.top+this.sep,this.w+10,30); //top pipe end
+        endShape();  
+    }
+
+    this.update = function(){
+        this.x-=4;
+    }
+
+    this.hitBird = function(bird){
+        if((bird.y < this.top || bird.y > this.top+this.sep)
+        && (bird.x > this.x && bird.x<this.x+this.w)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    this.randomize = function(){
+        this.top = random(height/4)*3;
+    }
+}
